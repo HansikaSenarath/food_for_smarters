aa4e580e6a75c52d30e14aad3847dc49f55ecf5e
HansiSenarath <your.email@example.com>
2024-12-27 12:08:02 +0530

    backend updated

--- Backend/config/Db.js
+++ Backend/config/Db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://hansikasenarath:HGsdatabaSe@cluster0.x5rl9.mongodb.net/foof-del');
-        console.log("DB Connected");
-    } catch (error) {
-        console.error("DB Connection Failed", error);
-        process.exit(1); // Exit the process with failure code
-    }
+  try {
+    await mongoose.connect(
+      "mongodb+srv://hansika:UJ7Qen9czzyGhdwR@cluster0.skhxh.mongodb.net/food-del"
+    );
+    console.log("DB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Exit the app if DB connection fails
+  }
 };
--- Backend/server.js
+++ Backend/server.js
@@ -1,31 +1,23 @@
-import express from "express";
-import cors from "cors";
-import { connectDB } from "./config/Db.js";
-import foodRouter from "./routes/FoodRoutes.js";
+import express from "express"
+import cors from "cors"
+import { connectDB } from "./config/db.js"
 
-// app config
-const app = express();
-const port = 4000;
+// appp config 
+const app = express()
+const port = 4000 
 
-// middleware
-app.use(express.json());
-app.use(cors());
-
-// db connection 
+// middleware 
+app.use(express.json())
+app.use(cors())
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+// DB connection 
 connectDB();
 
 
-// Api endpoint 
-app.use ("/api/food" , foodRouter)
-
-
-
-app.get("/", (req, res) => {
-    res.send("API Working");
-});
-
-app.listen(port, () => {
+// run express server 
+app.listen(port,()=>{
     console.log(`Server started on http://localhost:${port}`);
-});
-
+})
 
